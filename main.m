19425ed0b24f5cfa5a1787d70fa0853deba1665b
mpdannenberg <matthew-dannenberg@uiowa.edu>
2021-04-22 09:30:58 -0500

    Correct names and order of scripts

--- main.m
+++ main.m
@@ -6,15 +6,16 @@ calibrate_itrdb_parameters;
 % Calculate basic validation stats for VS-Lite model over independent
 % validation period 
 calculate_vslite_stats;
+calculate_vslite_warming_response;
 
 % Make validation maps
 make_vslite_r2_maps;
-make_vslite_r2_histograms;
+make_vslite_r2_table;
 
 % Get downscaled CMIP5 projections and simulate change in PET, soil
 % moisture, moisture stress, and growth
-get_bcsd_projections;
-calculate_vslite_bcsd_projections;
+get_bcsd_projections; % Takes a long time (more than an hour)
+calculate_vslite_bcsd_projections; % Takes a long time (several hours)
 plot_vslite_bcsd_projections; 
 supplemental_plot_vslite_bcsd_projections_rcp45; 
 plot_vslite_bcsd_gM_trajectory;
